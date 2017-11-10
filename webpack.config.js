69e9a2aeeb1f321ed197d0deaca44bdc780e938e
Colin Mackay <github@colinmackay.scot>
2017-11-10 09:57:19 +0000

    Html Web Pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,7 +9,11 @@ module.exports = {
       contentBase: './dist'
   },
   plugins: [
-      new CleanWebpackPlugin(['dist'])
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'November 2017 Scottish Government Tax Proposals',
+        template: 'src/index.html'
+      })
   ],
   output: {
     filename: 'bundle.js',
