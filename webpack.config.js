b97805d04aa1b37ba47fa59ce54488d4fcabc05c
Colin Mackay <github@colinmackay.scot>
2017-11-10 08:57:09 +0000

    Build source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'source-map',
   plugins: [
       new CleanWebpackPlugin(['dist'])
   ],
